0ac91e6692b78f7400f69aff7f194547e3556925
Valentin Perez <valpe70@gmail.com>
2016-03-21 13:35:21 -0400

    songs subset sql database exploration

--- process-xml.py
+++ process-xml.py
@@ -7,7 +7,8 @@ def main():
 	#p = vlc.MediaPlayer("file:///Users/valentin/Music/iTunes/iTunes%20Media/Music/Androma/Unknown%20Album/Anapo.mp3")
 	#p.play()
 	song = AudioSegment.from_mp3("somethingGood.mp3")
-	song.export("final.wav", format="wav")
+	first_ten_secs = song[:10*1000]
+	first_ten_secs.export("first10.wav", format="wav")
 
 if __name__ == '__main__':
     main()
